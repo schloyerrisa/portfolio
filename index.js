2f50b7a98fabf40a846fe058d7078c8fa0759b7f
Risa Schloyer <rschloyer@ucsd.edu>
2026-04-24 16:54:24 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = projects.slice(0, 3);
@@ -6,3 +6,5 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+const githubData = await fetchGitHubData('schloyerrisa');
