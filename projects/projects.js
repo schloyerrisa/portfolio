c0699303e83f5fb46b377613aadc1f67a6512f00
Risa Schloyer <rschloyer@ucsd.edu>
2026-04-24 16:42:32 -0700

    updated index.html and projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,6 +2,9 @@ import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
 
+const projectsTitle = document.querySelector('.projects-title');
+projectsTitle.textContent = `${projects.length} Projects`;
+
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
