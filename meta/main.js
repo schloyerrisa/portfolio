794c53442316abd6cc5c14de458274b6957da17c
Risa Schloyer <rschloyer@ucsd.edu>
2026-05-08 13:58:50 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -163,23 +163,35 @@ function renderScatterPlot(data, commits) {
     .attr('transform', `translate(${usableArea.left}, 0)`)
     .call(yAxis);
 
+    const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+
+    const rScale = d3
+    .scaleSqrt()
+    .domain([minLines, maxLines])
+    .range([3, 25]);
+
+    const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+
   // Draw dots
   const dots = svg.append('g').attr('class', 'dots');
 
   dots
   .selectAll('circle')
-  .data(commits)
+  .data(sortedCommits)
   .join('circle')
   .attr('cx', (d) => xScale(d.datetime))
   .attr('cy', (d) => yScale(d.hourFrac))
-  .attr('r', 5)
+  .attr('r', (d) => rScale(d.totalLines))
   .attr('fill', 'steelblue')
+  .style('fill-opacity', 0.7)
   .on('mouseenter', (event, commit) => {
+    d3.select(event.currentTarget).style('fill-opacity', 1);
     renderTooltipContent(commit);
     updateTooltipVisibility(true);
     updateTooltipPosition(event);
   })
-  .on('mouseleave', () => {
+  .on('mouseleave', (event) => {
+    d3.select(event.currentTarget).style('fill-opacity', 0.7);
     updateTooltipVisibility(false);
   });
 }
